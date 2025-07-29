6eb42e4a049ef20efa33fac82fbe9d7dbbced1e9
zai1208 <zai1208@outlook.com>
2025-07-29 22:04:19 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
 dotfiles_dir=~/dotfiles
 
+# Symlink .config
 ln -sf $dotfiles_dir/.config ~/.config
+
+# Copy Wallpapers
+mkdir -p ~/wallpapers
+cp -r $dotfiles_dir/wallpapers/* ~/wallpapers/
+
+echo "[*] Dotfiles deployment done. Ensure hyprland.conf has:"
+echo "    exec-once = swww init && swww img ~/wallpapers/endeavouros_gemini.png"
