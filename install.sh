97302e2d96048d8c8183a78a0752e240d7135e41
zai1208 <zai1208@outlook.com>
2025-07-31 09:13:47 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ dotfiles_dir=~/dotfiles
 # Symlink .config
 ln -sf $dotfiles_dir/.config ~/.config
 
-# Copy Wallpapers
-mkdir -p ~/wallpapers
-cp -r $dotfiles_dir/wallpapers/* ~/wallpapers/
+# Symlink wallpapers directory
+ln -sf $dotfiles_dir/wallpapers ~/wallpapers/
 
 echo "[*] Dotfiles deployment done."
