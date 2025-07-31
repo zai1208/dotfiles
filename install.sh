55efad611baabbf07531a296f9da059f159cb685
zai1208 <zai1208@outlook.com>
2025-07-31 18:59:14 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,22 @@
 
 dotfiles_dir=~/dotfiles
 
+# Remove all current dotfiles
+echo "[*] Removing current dotfiles"
+
+rm -rf ~/.config
+rm -rf ~/.bashrc
+rm -rf ~/trueline.sh
+rm -rf ~/wallpapers/
+
 # Symlink .config
 ln -sf $dotfiles_dir/.config ~/.config
 
 # Symlink wallpapers directory
 ln -sf $dotfiles_dir/wallpapers ~/wallpapers/
+
+# Symlinking other configs
+ln -sf $dotfiles_dir/.bashrc ~/.bashrc
+ln -sf $dotfiles_dir/trueline.sh ~/trueline.sh
 
 echo "[*] Dotfiles deployment done."
