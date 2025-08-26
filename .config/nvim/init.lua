01807ec5f30cc082f6c5c47f46f8c58375ff04d0
zai1208 <zai1208@outlook.com>
2025-08-26 14:59:43 +1000

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -231,8 +231,18 @@ vim.o.tabstop = 2       -- A tab character counts as 2 spaces
 vim.o.shiftwidth = 2    -- Indent commands (>> <<) use 2 spaces
 vim.o.expandtab = true  -- Convert tabs to spaces
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local transparent_groups = {
+  "Normal",
+  "NormalNC",
+  "NormalFloat",
+  "NeoTreeNormal",
+  "NeoTreeNormalNC",
+  "EndOfBuffer",
+}
+
+for _, group in ipairs(transparent_groups) do
+  vim.api.nvim_set_hl(0, group, { bg = "none" })
+end
 
 -- 4. Keymaps (non-whichkey)
 vim.g.mapleader = " "
