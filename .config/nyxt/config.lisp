e20702b548a3c3ced2dc5dc8e90cf6f4276f4db8
zai1208 <zai1208@outlook.com>
2026-02-25 12:57:51 +1100

    update

---
+++ .config/nyxt/config.lisp
@@ -0,0 +1,16 @@
+(define-configuration buffer
+		      ((default-modes
+			 (pushnew 'nyxt/mode/vi:vi-normal mode %slot-value%))))
+(define-configuration web-buffer
+		      ((default modes
+				(pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+(defvar *my-search-engines*
+  (list
+    '("google" "https://google.com/search?q=~a" "https://google.com")))
+
+(define-configuration context-buffer
+		      ((search-engines
+			 (append
+			   (mapcar (lamba (engine) (apply 'make-search-engine engine))
+				   *my-search-engines*)
+			   %slot-default%))))
